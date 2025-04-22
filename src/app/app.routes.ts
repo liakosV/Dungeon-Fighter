f68a79670396208ad037818e8b25f9c58966a348
LiakosV <billiak@hotmail.com>
2025-04-22 19:14:47 +0300

    feat: path routes created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignUpComponent}
+
+];
