23bf6b2e8fc5c33a83367faff4acd2e2c616f152
LiakosV <billiak@hotmail.com>
2025-04-22 19:15:28 +0300

    feat: background image created for reusability

---
+++ src/app/components/background-img/background-img.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-img',
+  imports: [],
+  templateUrl: './background-img.component.html',
+  styleUrl: './background-img.component.css'
+})
+export class BackgroundImgComponent {
+
+}
