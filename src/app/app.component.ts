fdffebd42b51bbd9c98caca04bb4a45e02329fc0
LiakosV <billiak@hotmail.com>
2025-04-22 19:12:03 +0300

    feat: router imports added and changed the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BackgroundImgComponent } from './components/background-img/background-img.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterModule],
+  // templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
