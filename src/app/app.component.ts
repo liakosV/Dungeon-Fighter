fa4874ed5ce6f1ebcc06d7089bba4226099ad909
LiakosV <billiak@hotmail.com>
2025-04-23 17:42:42 +0300

    feat: basic structure changed dashboard added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BackgroundImgComponent } from './components/background-img/background-img.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @Component({
@@ -12,7 +13,9 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     RouterModule,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BackgroundImgComponent,
+    SidebarComponent
   ],
   templateUrl: './app.component.html',
   // template: '<router-outlet></router-outlet>',
@@ -20,4 +23,11 @@ import { FooterComponent } from './components/footer/footer.component';
 })
 export class AppComponent {
   title = 'dungeon-fighter';
+
+  @ViewChild(SidebarComponent) sidebar!: SidebarComponent;
+
+  toggleSidebar() {
+    console.log("toggle called")
+    this.sidebar.toggle();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {path: '', component: WelcomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignUpComponent}
+  {path: 'signup', component: SignUpComponent},
+  {path: 'dashboard', component: DashboardComponent}
 
 ];
