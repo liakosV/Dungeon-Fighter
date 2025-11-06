f2df3cca5196b611756428d3d65329c42528d9dc
liakosV <billiak@hotmail.com>
2025-11-06 14:33:33 +0200

    feat: User repository created

---
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package gr.project.dungeonFighter.repository;
+
+import gr.project.dungeonFighter.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUuid(UUID uuid);
+}
