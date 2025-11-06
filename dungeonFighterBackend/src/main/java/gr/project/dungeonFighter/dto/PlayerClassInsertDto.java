6c372d081527d20ef3aba3fcd5530a5ac4cdb4bb
liakosV <billiak@hotmail.com>
2025-11-06 11:31:52 +0200

    feat: Player class insert dto created

---
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/dto/PlayerClassInsertDto.java
@@ -0,0 +1,18 @@
+package gr.project.dungeonFighter.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class PlayerClassInsertDto {
+
+    @NotEmpty(message = "The name cannot be empty.")
+    private String name;
+
+    @NotEmpty(message = "The description cannot be empty.")
+    private String description;
+
+    @NotEmpty(message = "The class type cannot be empty.")
+    private String classType;
+}
