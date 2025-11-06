57f5f2cac297161df75eb6719e39add244d505ac
liakosV <billiak@hotmail.com>
2025-11-06 14:33:54 +0200

    feat: Player repository created

---
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/repository/PlayerRepository.java
@@ -0,0 +1,13 @@
+package gr.project.dungeonFighter.repository;
+
+import gr.project.dungeonFighter.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    Optional<Player> findByUuid(UUID uuid);
+    Optional<Player> findByName(String name);
+}
