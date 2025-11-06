67b7e0717ed8ec2d1f0d40d8ac6a5ee6e6b00fcb
liakosV <billiak@hotmail.com>
2025-11-06 11:32:13 +0200

    feat: Stats insert dto created

---
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/dto/StatsInsertDto.java
@@ -0,0 +1,14 @@
+package gr.project.dungeonFighter.dto;
+
+import lombok.Data;
+
+@Data
+public class StatsInsertDto {
+
+    private int strength;
+    private int intelligence;
+    private int stamina;
+    private int dexterity;
+    private int defense;
+
+}
