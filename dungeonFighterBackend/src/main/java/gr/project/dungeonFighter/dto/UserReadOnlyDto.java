42025b92fef9c35d1444c557acfb8c8fffe73e19
liakosV <billiak@hotmail.com>
2025-11-06 14:36:52 +0200

    feat: uuid field added fix: id field commented out

--- dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/dto/UserReadOnlyDto.java
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/dto/UserReadOnlyDto.java
@@ -2,10 +2,13 @@ package gr.project.dungeonFighter.dto;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class UserReadOnlyDto {
 
-    private Long id;
+//    private Long id;
+    private UUID uuid;
     private String username;
     private String email;
     private Boolean isActive;
