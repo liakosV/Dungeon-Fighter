5458cf19133d8f6702f7c051d5587c5d941d36e4
liakosV <billiak@hotmail.com>
2025-11-09 14:22:39 +0200

    feat: AppObjectNotAuthorizedException created

---
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/core/exceptions/AppObjectNotAuthorizedException.java
@@ -0,0 +1,8 @@
+package gr.project.dungeonFighter.core.exceptions;
+
+public class AppObjectNotAuthorizedException extends AppGenericException {
+  private static final String DEFAULT_CODE = "NotAuthorized";
+    public AppObjectNotAuthorizedException(String code, String message) {
+        super(code + DEFAULT_CODE, message);
+    }
+}
