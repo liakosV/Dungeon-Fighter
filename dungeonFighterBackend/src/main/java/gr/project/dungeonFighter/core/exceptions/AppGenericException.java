5fc8849ef3be8e939c9f74e3ffb7f85b7f8f99ff
liakosV <billiak@hotmail.com>
2025-11-09 14:21:53 +0200

    feat: AppGenericException created

---
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/core/exceptions/AppGenericException.java
@@ -0,0 +1,12 @@
+package gr.project.dungeonFighter.core.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class AppGenericException extends RuntimeException {
+    private final String code;
+    public AppGenericException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
