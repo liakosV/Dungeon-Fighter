b71d0b5a33874638803cff496c402ebed7ac4fcf
liakosV <billiak@hotmail.com>
2025-11-13 19:03:29 +0200

    feat: add EnableJpaAuditing annotation

--- dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/DungeonFighterApplication.java
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/DungeonFighterApplication.java
@@ -2,8 +2,10 @@ package gr.project.dungeonFighter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DungeonFighterApplication {
 
 	public static void main(String[] args) {
