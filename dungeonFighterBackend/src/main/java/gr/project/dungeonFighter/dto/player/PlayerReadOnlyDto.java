97c130d94dbb10e460727ef3bfd63594879beef8
liakosV <billiak@hotmail.com>
2025-11-13 19:27:04 +0200

    refactor: remove commented id field and change StatsReadOnlyDto field to StatsDto

--- dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/dto/player/PlayerReadOnlyDto.java
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/dto/player/PlayerReadOnlyDto.java
@@ -1,18 +1,17 @@
 package gr.project.dungeonFighter.dto.player;
 
-import gr.project.dungeonFighter.dto.stats.StatsReadOnlyDto;
+import gr.project.dungeonFighter.dto.stats.StatsDto;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
 public class PlayerReadOnlyDto {
-//    private Long id;
     private UUID uuid;
     private String name;
     private String playerClassName;
     private int level;
     private int xp;
     private int gold;
-    private StatsReadOnlyDto stats;
+    private StatsDto stats;
 }
