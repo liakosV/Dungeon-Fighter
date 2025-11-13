8b819332826aa3093114240b48f6cf492938017a
liakosV <billiak@hotmail.com>
2025-11-13 19:15:45 +0200

    refactor: remove @Mapping for classType from toReadOnlyDto and toEntity feat: add toUpdate method

--- dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/mapper/PlayerClassMapper.java
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/mapper/PlayerClassMapper.java
@@ -2,16 +2,19 @@ package gr.project.dungeonFighter.mapper;
 
 import gr.project.dungeonFighter.dto.playerClass.PlayerClassInsertDto;
 import gr.project.dungeonFighter.dto.playerClass.PlayerClassReadOnlyDto;
+import gr.project.dungeonFighter.dto.playerClass.PlayerClassUpdateDto;
 import gr.project.dungeonFighter.model.static_data.PlayerClass;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = StatsMapper.class)
 public interface PlayerClassMapper {
 
-    @Mapping(target = "classType", expression = "java(playerClass.getType().name())")
     PlayerClassReadOnlyDto toReadOnlyDto(PlayerClass playerClass);
 
-    @Mapping(target = "classType", expression = "java(ClassType.valueOf(dto.getClassType().toUpperCase()))")
+    @Mapping(target = "stats", source = "stats")
     PlayerClass toEntity(PlayerClassInsertDto dto);
+
+    @Mapping(target = "stats", source = "stats")
+    void toUpdate(PlayerClassUpdateDto dto, PlayerClass entity);
 }
