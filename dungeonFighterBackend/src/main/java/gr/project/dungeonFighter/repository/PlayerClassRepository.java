3982f27e1d3574218344dbcce6e3e173aa91f0d5
liakosV <billiak@hotmail.com>
2025-11-13 19:18:37 +0200

    feat: add findByUuid method

--- dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/repository/PlayerClassRepository.java
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/repository/PlayerClassRepository.java
@@ -4,7 +4,9 @@ import gr.project.dungeonFighter.model.static_data.PlayerClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface PlayerClassRepository extends JpaRepository<PlayerClass, Long> {
     Optional<PlayerClass> findByName(String name);
+    Optional<PlayerClass> findByUuid(UUID uuid);
 }
