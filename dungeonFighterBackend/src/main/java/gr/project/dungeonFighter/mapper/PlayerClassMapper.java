2c02e7b5cdc7f1bdb3d01ed4299e198064206c57
liakosV <billiak@hotmail.com>
2025-11-14 21:04:53 +0200

    refactor: update PlayerClassMapper to include StatsGrowthMapper and improve null value handling

--- dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/mapper/PlayerClassMapper.java
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/mapper/PlayerClassMapper.java
@@ -3,18 +3,22 @@ package gr.project.dungeonFighter.mapper;
 import gr.project.dungeonFighter.dto.playerClass.PlayerClassInsertDto;
 import gr.project.dungeonFighter.dto.playerClass.PlayerClassReadOnlyDto;
 import gr.project.dungeonFighter.dto.playerClass.PlayerClassUpdateDto;
-import gr.project.dungeonFighter.model.static_data.PlayerClass;
+import gr.project.dungeonFighter.model.PlayerClass;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring", uses = StatsMapper.class)
+@Mapper(componentModel = "spring", uses = {StatsMapper.class, StatsGrowthMapper.class})
 public interface PlayerClassMapper {
 
     PlayerClassReadOnlyDto toReadOnlyDto(PlayerClass playerClass);
 
-    @Mapping(target = "stats", source = "stats")
+//    @Mapping(target = "stats", source = "stats")
+    @Mapping(target = "growth", source = "growthDto")
     PlayerClass toEntity(PlayerClassInsertDto dto);
 
-    @Mapping(target = "stats", source = "stats")
-    void toUpdate(PlayerClassUpdateDto dto, PlayerClass entity);
+//    @Mapping(target = "stats", source = "stats", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "growth", source = "growthDto", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void toUpdate(PlayerClassUpdateDto dto, @MappingTarget PlayerClass entity);
 }
