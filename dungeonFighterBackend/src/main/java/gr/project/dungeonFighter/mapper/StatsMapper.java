ab3a39a5710948301f15690e133116564cb3338d
liakosV <billiak@hotmail.com>
2025-11-14 21:06:47 +0200

    feat: add copy method to StatsMapper for duplicating Stats objects

--- dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/mapper/StatsMapper.java
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/mapper/StatsMapper.java
@@ -1,7 +1,7 @@
 package gr.project.dungeonFighter.mapper;
 
 import gr.project.dungeonFighter.dto.stats.StatsDto;
-import gr.project.dungeonFighter.model.Stats;
+import gr.project.dungeonFighter.model.static_data.Stats;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -10,4 +10,15 @@ public interface StatsMapper {
     StatsDto toDto(Stats stats);
 
     Stats toEntity(StatsDto dto);
+
+    default Stats copy(Stats stats){
+        if (stats == null) return null;
+        Stats copy = new Stats();
+        copy.setStrength(stats.getStrength());
+        copy.setIntelligence(stats.getIntelligence());
+        copy.setStamina(stats.getStamina());
+        copy.setDexterity(stats.getDexterity());
+        copy.setDefense(stats.getDefense());
+        return copy;
+    }
 }
