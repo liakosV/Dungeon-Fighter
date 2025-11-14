c0731daa012f986aee87ea0679c6fad69ba34c98
liakosV <billiak@hotmail.com>
2025-11-14 21:06:33 +0200

    feat: create StatsGrowthDto class with growth attribute fields

---
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/dto/stats/StatsGrowthDto.java
@@ -0,0 +1,12 @@
+package gr.project.dungeonFighter.dto.stats;
+
+import lombok.Data;
+
+@Data
+public class StatsGrowthDto {
+    private double strengthGrowth;
+    private double intelligenceGrowth;
+    private double staminaGrowth;
+    private double dexterityGrowth;
+    private double defenseGrowth;
+}
