5ebac799f534267c423c4ea988219ce9b002d914
liakosV <billiak@hotmail.com>
2025-11-14 21:06:54 +0200

    refactor: update import to reflect Stats relocation to static_data package

--- dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/repository/StatsRepository.java
+++ dungeonFighterBackend/src/main/java/gr/project/dungeonFighter/repository/StatsRepository.java
@@ -1,6 +1,6 @@
 package gr.project.dungeonFighter.repository;
 
-import gr.project.dungeonFighter.model.Stats;
+import gr.project.dungeonFighter.model.static_data.Stats;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StatsRepository extends JpaRepository<Stats, Long> {
